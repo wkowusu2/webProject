79fcf3a8dc748019658953e79eedcc2854d8d3b0
Zahemen <zahemen9900@gmail.com>
2025-03-26 14:11:43 +0000

    Updated hospital image URL in Hospitals component for improved visual representation

--- src/ActualApp/Hospitals.jsx
+++ src/ActualApp/Hospitals.jsx
@@ -54,7 +54,7 @@ const Hospitals = () => {
       location: "Tema, Greater Accra",
       phone: "+233 303-999-999",
       email: "contact@temageneral.com",
-      imageUrl: "https://images.unsplash.com/photo-1626315869436-d6989dd83277?ixlib=rb-4.0.3",
+      imageUrl: "https://images.unsplash.com/photo-1565262353342-6e919eab5b58?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       services: [
         "General Medicine",
         "Pediatrics",
